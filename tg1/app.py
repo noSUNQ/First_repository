6f2a234550a1b6fddee55203371698367afff40f
noSUNQ <alchin.vadim@icloud.com>
2025-12-30 09:45:28 +0000

    #2

---
+++ tg1/app.py
@@ -0,0 +1,10 @@
+From flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
