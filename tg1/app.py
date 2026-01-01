cc309a39088c98e5400856afeff421be26068d59
Vadim <alchin.vadim@icloud.com>
2026-01-01 15:50:51 +0300

    Add .gitignore

--- tg1/app.py
+++ tg1/app.py
@@ -1,10 +1,10 @@
-From flask import Flask, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    render_template("index.html")
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
